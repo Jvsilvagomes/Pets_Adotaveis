ad52982ef794ae3b30d461da634af4a366e17587
joao <joao.silvassenai@gmail.com>
2025-09-21 22:49:05 -0300

    Preenche dados com 10 pets

---
+++ src/models/dados.js
@@ -0,0 +1,91 @@
+const pets = [
+    {
+        id: 1,
+        nome: "Zeus",
+        especie: "cachorro",
+        raça: "Pitbull",
+        idade: 12,
+        peso: 21,
+    },
+    {
+        id: 2,
+        nome: "Luna",
+        especie: "gato",
+        raça: "Siamês",
+        idade: 4,
+        peso: 5,
+        disponivel: true
+    },
+    {
+        id: 3,
+        nome: "Thor",
+        especie: "cachorro",
+        raça: "Labrador",
+        idade: 7,
+        peso: 32,
+        disponivel: false
+    },
+    {
+        id: 4,
+        nome: "Mel",
+        especie: "cachorro",
+        raça: "Poodle",
+        idade: 3,
+        peso: 7,
+        disponivel: true
+    },
+    {
+        id: 5,
+        nome: "Kiara",
+        especie: "gato",
+        raça: "Persa",
+        idade: 5,
+        peso: 6,
+        disponivel: true
+    },
+    {
+        id: 6,
+        nome: "Apolo",
+        especie: "cachorro",
+        raça: "Husky Siberiano",
+        idade: 6,
+        peso: 28,
+        disponivel: false
+    },
+    {
+        id: 7,
+        nome: "Bob",
+        especie: "cachorro",
+        raça: "Vira-lata",
+        idade: 2,
+        peso: 12,
+        disponivel: true
+    },
+    {
+        id: 8,
+        nome: "Nina",
+        especie: "gato",
+        raça: "Maine Coon",
+        idade: 8,
+        peso: 9,
+        disponivel: false
+    },
+    {
+        id: 9,
+        nome: "Amora",
+        especie: "coelho",
+        raça: "Lionhead",
+        idade: 1,
+        peso: 2,
+        disponivel: true
+    },
+    {
+        id: 10,
+        nome: "Fred",
+        especie: "cachorro",
+        raça: "Beagle",
+        idade: 9,
+        peso: 14,
+        disponivel: true
+    }
+]
